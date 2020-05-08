4f10c2b2f483edded30215c7db87f4938097abb4
Ching Chen <stone2809@gmail.com>
2020-05-08 23:01:23 +0800

    add csv

--- products.py
+++ products.py
@@ -6,3 +6,11 @@ while True:
 	price = input('Price:')
 	products.append([name, price])
 print(products)
+
+for p in products:
+	print(p[0] + '的價格是' + p[1])
+
+with open('products.csv', 'w', encoding= 'utf-8') as f:
+	f.write('Products,Price' + '\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
